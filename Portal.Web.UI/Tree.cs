3e415043a36ec79bd05a0be12a004dc5c3f4dd13
Ariel Carmely <arielcrm@gmail.com>
2015-08-30 13:59:37 +0300

    fdsgf

---
+++ Portal.Web.UI/Tree.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Portal.Web.UI
+{
+    public class Tree
+    {
+        private List<TreeNode> nodes;
+        public List<TreeNode> Nodes
+        {
+            get
+            {
+                if (this.nodes == null)
+                {
+                    this.nodes = new List<TreeNode>();
+                }
+
+                return this.nodes;
+            }
+            set
+            {
+                this.nodes = value;
+            }
+        }
+
+        public string RootTitle { get; set; }
+
+        public void AddNode(TreeNode node)
+        {
+            this.Nodes.Add(node);
+            node.Tree = this;
+        }
+
+    }
+}
---
+++ Portal.Web.UI/TreeNode.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Portal.Web.UI
+{
+    public class TreeNode
+    {
+        private List<TreeNode> childNodes;
+
+        public TreeNode Parent { get; set; }
+        public Tree Tree { get; set; }
+        public string ThumbImageUrl { get; set; }
+        public string Title { get; set; }
+        public string Caption { get; set; }
+        public string Description { get; set; }
+        public string Action { get; set; }
+        public string Controller { get; set; }
+        public Int64 Id { get; set; }
+
+        public List<TreeNode> ChildNodes
+        {
+            get
+            {
+                if (this.childNodes == null)
+                {
+                    this.childNodes = new List<TreeNode>();
+                }
+
+                return this.childNodes;
+            }
+            set
+            {
+                this.childNodes = value;
+            }
+        }
+
+        public TreeNode() { }
+
+        public void AddChild(TreeNode node)
+        {
+            this.ChildNodes.Add(node);
+            node.Tree = this.Tree;
+            node.Parent = this;
+        }
+    }
+}
