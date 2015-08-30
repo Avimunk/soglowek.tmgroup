dc410b5a3b2c5889b05df4875ea5759671e2b9f8
Ariel Carmely <arielcrm@gmail.com>
2015-08-30 13:59:14 +0300

    fgh

---
+++ Portal.Clients.Test/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Portal.Clients;
+
+namespace Portal.Clients.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SSOClient ssoClient = new SSOClient();
+
+            string ssoLoginName = ssoClient.GetCurrentLoginName();
+            if (!String.IsNullOrEmpty(ssoLoginName))
+            {
+                string ssoPropertyName = "st";
+
+                if (!String.IsNullOrEmpty(ssoPropertyName))
+                {
+                    string ssoPropertyValue = ssoClient.GetProperty(ssoLoginName, ssoPropertyName);
+
+                    if (!String.IsNullOrEmpty(ssoPropertyValue))
+                    {
+                        Int64 localId = Int64.Parse(ssoPropertyValue);
+
+                        if (localId > 0)
+                        {
+                            Console.WriteLine("ID: " + localId);
+                            Console.ReadLine();
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
